81949b2d462c91f6fafc5f73acb6750e33888e4a
Glitch (hello-webpage) <none>
2020-06-23 17:26:53 -0300

    pixi test 28

--- itsrainingh/script.js
+++ itsrainingh/script.js
@@ -23,7 +23,7 @@ app.loader.add('plane', 'h.png').load((loader, resources) => {
     plane.anchor.x = 0.5;
     plane.anchor.y = 1;
 
-    plane.scale.x = Math.random() / 2;
+    plane.scale.x = Math.random() / 5;
     plane.scale.y = plane.scale.x;
     objs.push(plane)
     // Add the bunny to the scene we are building
